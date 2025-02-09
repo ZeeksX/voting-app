4c961edb3d09b86ea4ddb27d66bdf593a86c41a7
ZeeksX <ikinwotezekiel@gmail.com>
2025-02-09 10:20:44 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import HomePage from './pages/HomePage';
-import Login from "./pages/Login";
 import { AuthProvider, useAuth } from './components/Auth';
-import SignUp from './pages/Signup';
 import ParticlesReact from './pages/ParticlesReact';
 import "./App.css"
 import "./index.css"
@@ -36,15 +33,6 @@ const App = () => {
       <Router>
         <Routes>
           <Route path='/' element={<ParticlesReact />} />
-          <Route path="/login" element={<Login isMobile={isMobile} />} />
-          <Route path="/dashboard" element={
-            // <ProtectedRoute>
-              <HomePage sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} isMobile={isMobile} />
-            // </ProtectedRoute>
-          } />
-          <Route path="/signup" element={
-            <SignUp sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} isMobile={isMobile} />
-          } />
         </Routes>
       </Router>
     </AuthProvider>
